aabcff55835e791980fa588d41053c297e814414
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-10-21 18:37:44 +0600

    Organized CSS classes

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -5,13 +5,11 @@ import './contact-us-page.css';
 
 // Sections
 import ContactUsHero from '@/components/Site/Pages/ContactUsPage/ContactUsHero/ContactUsHero';
-import ServicesWeOfferOverview from '@/components/Site/Pages/HomePage/ServicesWeOfferOverview/ServicesWeOfferOverview';
 
 const ContactUsPage: FC = () => {
   return (
     <main className="contact-us-page">
       <ContactUsHero />
-      <ServicesWeOfferOverview />
     </main>
   );
 };
