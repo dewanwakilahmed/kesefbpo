b018ac5f91f352c6d1c8e355ba99ab9a718cd859
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-11-02 20:39:05 +0600

    Created CompanyContactInformation section

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -6,12 +6,14 @@ import './contact-us-page.css';
 // Sections
 import ContactUsHero from '@/components/Site/Pages/ContactUsPage/ContactUsHero/ContactUsHero';
 import ContactUsForm from '@/components/Site/Pages/ContactUsPage/ContactUsForm/ContactUsForm';
+import CompanyContactInformation from '@/components/Site/Pages/ContactUsPage/CompanyContactInformation/CompanyContactInformation';
 
 const ContactUsPage: FC = () => {
   return (
     <main className="contact-us-page">
       <ContactUsHero />
       <ContactUsForm />
+      <CompanyContactInformation />
     </main>
   );
 };
---
+++ components/Site/Pages/ContactUsPage/CompanyContactInformation/CompanyContactInformation.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+// CSS
+import './company-contact-information.css';
+
+const CompanyContactInformation: FC = () => {
+  return (
+    <section className="company-contact-information">
+      Company Contact Information
+    </section>
+  );
+};
+
+export default CompanyContactInformation;
