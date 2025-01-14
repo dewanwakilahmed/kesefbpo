f03938925c3f56f06d65e1b1014d5fd1be606a71
dewanwakilahmed <dewanwakilahmed@gmail.com>
2025-01-14 08:30:31 +0600

    Created AboutUsHero section

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -7,7 +7,11 @@ import './about-us-page.css';
 import AboutUsHero from '@/components/Site/Pages/AboutUsPage/AboutUsHero/AboutUsHero';
 
 const AboutUsPage: FC = () => {
-  return <main className="about-us-page">{/* <AboutUsHero /> */}</main>;
+  return (
+    <main className="about-us-page">
+      <AboutUsHero />
+    </main>
+  );
 };
 
 export default AboutUsPage;
--- components/Site/Pages/AboutUsPage/AboutUsHero/AboutUsHero.tsx
+++ components/Site/Pages/AboutUsPage/AboutUsHero/AboutUsHero.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 
 // Font
-import { primaryFont } from '@/public/fonts/fonts';
+import { secondaryFont } from '@/public/fonts/fonts';
 
 // CSS
 import './about-us-hero.css';
@@ -11,18 +11,32 @@ const AboutUsHero: FC = () => {
   return (
     <section className="about-us-hero">
       <div className="about-us-hero-container">
-        <h1 className={`about-us-hero-headline ${primaryFont.className}`}>
-          Your Trusted Partner in Business Growth
+        <h1 className={`about-us-hero-headline ${secondaryFont.className}`}>
+          Empowering Businesses with Outsourcing Excellence
         </h1>
-        <h2 className={`about-us-hero-subheadline ${primaryFont.className}`}>
-          Driven by Excellence, Focused on Your Success
+        <h2 className={`about-us-hero-subheadline ${secondaryFont.className}`}>
+          Discover Our Story, Values, and Commitment to Your Success
         </h2>
         <p className="about-us-hero-description">
-          At Kesef BPO, we empower businesses to achieve more. Our team combines
-          expertise and dedication to streamline your operations, allowing you
-          to focus on what matters most—your growth.
+          At Kesef BPO, we&apos;re more than just an outsourcing partner —
+          we&apos;re an extension of your team. Our mission is to help your
+          business thrive by providing innovative solutions, unmatched
+          expertise, and a dedication to delivering results
         </p>
-        <div className="about-us-hero-cta"></div>
+        <div className="about-us-hero-cta">
+          <Link
+            href="/about-us/meet-the-team"
+            className={`about-us-hero-btn btn btn-primary ${secondaryFont.className}`}
+          >
+            Meet the Team
+          </Link>
+          <Link
+            href="/about-us/company-culture"
+            className={`about-us-hero-btn btn btn-secondary ${secondaryFont.className}`}
+          >
+            Company Culture
+          </Link>
+        </div>
       </div>
     </section>
   );
