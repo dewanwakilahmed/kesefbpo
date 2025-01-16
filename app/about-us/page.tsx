9f04149c16a220006d5adafafdf3031f491410d1
dewanwakilahmed <dewanwakilahmed@gmail.com>
2025-01-16 09:35:57 +0600

    Created About Us Introduction section

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,11 +5,13 @@ import './about-us-page.css';
 
 // Sections
 import AboutUsHero from '@/components/Site/Pages/AboutUsPage/AboutUsHero/AboutUsHero';
+import AboutUsIntroduction from '@/components/Site/Pages/AboutUsPage/AboutUsIntroduction/AboutUsIntroduction';
 
 const AboutUsPage: FC = () => {
   return (
     <main className="about-us-page">
       <AboutUsHero />
+      <AboutUsIntroduction />
     </main>
   );
 };
---
+++ components/Site/Pages/AboutUsPage/AboutUsIntroduction/AboutUsIntroduction.tsx
@@ -0,0 +1,55 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+
+// Font
+import { secondaryFont } from '@/public/fonts/fonts';
+
+// CSS
+import './about-us-introduction.css';
+
+const AboutUsIntroduction: FC = () => {
+  return (
+    <section className="about-us-introduction">
+      <div className="about-us-introduction-container">
+        <h2
+          className={`about-us-introduction-heading ${secondaryFont.className}`}
+        >
+          Driving Business Growth with Tailored BPO Solutions
+        </h2>
+        <p className="about-us-introduction-description">
+          Comprehensive outsourcing services designed to streamline operations
+          and fuel your business success
+        </p>
+
+        <div className="about-us-introduction-box">
+          <div className="about-us-introduction-img-box">
+            <Image
+              className="about-us-introduction-img"
+              src="/images/about-us-introduction.webp"
+              alt=""
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div className="about-us-introduction-text-box">
+            <p className="about-us-introduction-text">
+              At KESEF BPO, we specialize in delivering comprehensive business
+              process outsourcing solutions designed to help businesses thrive.
+              Our current services include Lead Generation, Sales &
+              Telemarketing, Back Office Support, Virtual Assistance, and
+              Customer Support.
+            </p>
+            <p className="about-us-introduction-text">
+              Looking ahead, we&apos;re excited to gradually expand into IT &
+              Technical Support, Web Development, App Development, and Digital
+              Marketing, ensuring we provide holistic solutions to support your
+              business at every stage of growth.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUsIntroduction;
