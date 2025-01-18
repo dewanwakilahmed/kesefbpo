798cab642ab8f1d66c6efc321eb06cd6794bf65f
dewanwakilahmed <dewanwakilahmed@gmail.com>
2025-01-18 12:46:47 +0600

    Renamed Mission Vision Section

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -6,14 +6,14 @@ import './about-us-page.css';
 // Sections
 import AboutUsHero from '@/components/Site/Pages/AboutUsPage/AboutUsHero/AboutUsHero';
 import AboutUsIntroduction from '@/components/Site/Pages/AboutUsPage/AboutUsIntroduction/AboutUsIntroduction';
-import AboutUsMissionVision from '@/components/Site/Pages/AboutUsPage/AboutUsMissionVision/AboutUsMissionVision';
+import MissionVision from '@/components/Site/Pages/AboutUsPage/MissionVision/MissionVision';
 
 const AboutUsPage: FC = () => {
   return (
     <main className="about-us-page">
       <AboutUsHero />
       <AboutUsIntroduction />
-      <AboutUsMissionVision />
+      <MissionVision />
     </main>
   );
 };
--- components/Site/Pages/AboutUsPage/MissionVision/MissionVision.tsx
+++ components/Site/Pages/AboutUsPage/MissionVision/MissionVision.tsx
@@ -4,36 +4,34 @@ import React, { FC } from 'react';
 import { secondaryFont } from '@/public/fonts/fonts';
 
 // CSS
-import './about-us-mission-vision.css';
+import './mission-vision.css';
 
 const AboutUsMissionVision: FC = () => {
   return (
-    <section className="about-us-mission-vision">
-      <div className="about-us-mission-vision-container">
-        <h2
-          className={`about-us-mission-vision-heading ${secondaryFont.className}`}
-        >
+    <section className="mission-vision">
+      <div className="mission-vision-container">
+        <h2 className={`mission-vision-heading ${secondaryFont.className}`}>
           Empowering Businesses, Enriching Communities
         </h2>
-        <p className="about-us-mission-vision-description">
+        <p className="mission-vision-description">
           At KESEF BPO, our mission drives us daily, and our vision shapes the
           future we’re building—for businesses and the community alike
         </p>
-        <div className="about-us-mission-vision-content">
-          <div className="about-us-mission">
-            <div className="about-us-mission-content">
-              <h3 className="about-us-mission-heading">Our Mission</h3>
-              <p className="about-us-mission-statement">
+        <div className="mission-vision-content">
+          <div className="mission">
+            <div className="mission-content">
+              <h3 className="mission-heading">Our Mission</h3>
+              <p className="mission-statement">
                 To empower businesses with seamless outsourcing solutions that
                 drive efficiency, innovation, and sustainable growth while
                 fostering a skilled, modern workforce that uplifts the community
               </p>
             </div>
           </div>
-          <div className="about-us-vision">
-            <div className="about-us-vision-content">
-              <h3 className="about-us-vision-heading">Our Vision</h3>
-              <p className="about-us-vision-statement">
+          <div className="vision">
+            <div className="vision-content">
+              <h3 className="vision-heading">Our Vision</h3>
+              <p className="vision-statement">
                 To become a global leader in Business Process Outsourcing by
                 expanding our services and cultivating a dynamic workforce that
                 strengthens businesses and enriches the communities we serve
