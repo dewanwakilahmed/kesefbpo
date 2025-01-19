b2c7cf520050837920709faad45fa4da498068da
dewanwakilahmed <dewanwakilahmed@gmail.com>
2025-01-19 12:49:44 +0600

    Minor styles adjustment

--- components/Site/Pages/AboutUsPage/WhyChooseUs/WhyChooseUs.tsx
+++ components/Site/Pages/AboutUsPage/WhyChooseUs/WhyChooseUs.tsx
@@ -4,7 +4,10 @@ import React, { FC } from 'react';
 import { secondaryFont } from '@/public/fonts/fonts';
 
 // Icons
-import { FaCogs, FaUserTie, FaRocket, FaHandshake } from 'react-icons/fa';
+import { HiOutlineAdjustments } from 'react-icons/hi';
+import { FiAward } from 'react-icons/fi';
+import { MdOutlineTrendingUp } from 'react-icons/md';
+import { AiOutlineTeam } from 'react-icons/ai';
 
 // CSS
 import './why-choose-us.css';
@@ -14,22 +17,22 @@ const whyChooseUsContent = [
   {
     heading: 'Tailored Solutions for Every Business',
     text: 'We understand that no two businesses are alike. That’s why our services are customized to meet the unique needs of your organization, ensuring maximum efficiency and impact',
-    icon: <FaCogs />,
+    icon: <HiOutlineAdjustments />,
   },
   {
     heading: 'Unmatched Expertise and Commitment',
     text: 'Our team of dedicated professionals combines industry experience with a passion for excellence. We work as an extension of your team, delivering results you can count on',
-    icon: <FaUserTie />,
+    icon: <FiAward />,
   },
   {
     heading: 'Future-Ready Services',
     text: 'From lead generation to back-office support, we offer comprehensive solutions that grow with your business. With plans to expand into IT support, digital marketing, and more, we ensure you’re always ahead in a competitive market',
-    icon: <FaRocket />,
+    icon: <MdOutlineTrendingUp />,
   },
   {
     heading: 'Client-Centric Approach',
     text: 'Your success is our priority. We take pride in building long-term relationships, driven by transparency, trust, and a shared commitment to achieving your goals',
-    icon: <FaHandshake />,
+    icon: <AiOutlineTeam />,
   },
 ];
 
